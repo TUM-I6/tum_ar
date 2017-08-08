a0eb5aa9829d329fae0018bddcbb52aa5a49a1de
Arne Peters <arne.peters@tum.de>
2017-08-08 12:04:21 +0200

    fixed compile errors, caused by renaming of the project

--- tum_ar_window/src/mainwindow.cpp
+++ tum_ar_window/src/mainwindow.cpp
@@ -36,8 +36,8 @@ void MainWindow::displayImage(const std::string& url) {
 	//sleep(3) ;
 }
 
-void MainWindow::executeARInspection() { // const ar_window::ARInspectionGoalConstPtr &goal
-	ar_window::ARInspectionGoalConstPtr goal = _actionServer.acceptNewGoal() ;
+void MainWindow::executeARInspection() { // const tum_ar_window::ARInspectionGoalConstPtr &goal
+	tum_ar_window::ARInspectionGoalConstPtr goal = _actionServer.acceptNewGoal() ;
 
 
 	_wsaType = goal->wsa_type ;
@@ -56,12 +56,12 @@ void MainWindow::executeARInspection() { // const ar_window::ARInspectionGoalCon
 	displayImage(getImageURL(_wsaType, _pois[_pos])) ;
 
 	// publish info to the console for the user
-	ROS_INFO_STREAM("[ar_window] Running AR inspection on "<<_pois.size()<<" POIs of "<<_wsaType<<" WSA") ;
+	ROS_INFO_STREAM("[tum_ar_window] Running AR inspection on "<<_pois.size()<<" POIs of "<<_wsaType<<" WSA") ;
 }
 
 void MainWindow::pushButtonAcceptClicked() {
-	ROS_INFO_STREAM("[ar_window] POI "<<_pois[_pos]<<" accepted.") ;
-	publishFeedback(_pois[_pos], ar_window::ARInspectionFeedback::ACCEPTED) ;
+	ROS_INFO_STREAM("[tum_ar_window] POI "<<_pois[_pos]<<" accepted.") ;
+	publishFeedback(_pois[_pos], tum_ar_window::ARInspectionFeedback::ACCEPTED) ;
 
 	_pos++ ;
 	if (_pos >= _pois.size()) {
@@ -69,8 +69,8 @@ void MainWindow::pushButtonAcceptClicked() {
 		displayImage(getImageURL()) ;
 		_pois.clear() ;
 
-		ar_window::ARInspectionResult result ;
-		result.status = ar_window::ARInspectionResult::ACCEPTED ;
+		tum_ar_window::ARInspectionResult result ;
+		result.status = tum_ar_window::ARInspectionResult::ACCEPTED ;
 		_actionServer.setSucceeded(result);
 	}
 	else {
@@ -79,14 +79,14 @@ void MainWindow::pushButtonAcceptClicked() {
 }
 
 void MainWindow::pushButtonRejectClicked() {
-	ROS_INFO_STREAM("[ar_window] POI "<<_pois[_pos]<<" rejected.") ;
-	publishFeedback(_pois[_pos], ar_window::ARInspectionFeedback::REJECTED) ;
+	ROS_INFO_STREAM("[tum_ar_window] POI "<<_pois[_pos]<<" rejected.") ;
+	publishFeedback(_pois[_pos], tum_ar_window::ARInspectionFeedback::REJECTED) ;
 
 	displayImage(getImageURL()) ;
 	_pos = 0 ;
 	_pois.clear() ;
 	
-	ar_window::ARInspectionResult result ;
-	result.status = ar_window::ARInspectionResult::REJECTED ;
+	tum_ar_window::ARInspectionResult result ;
+	result.status = tum_ar_window::ARInspectionResult::REJECTED ;
 	_actionServer.setSucceeded(result);
 }

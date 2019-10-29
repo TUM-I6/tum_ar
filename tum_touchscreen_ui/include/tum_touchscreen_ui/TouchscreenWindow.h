7f9517647d6073f54069a3da759f962f0c1140f5
Arne <arne.peters@tum.de>
2019-10-29 11:46:43 +0100

    button types

--- tum_touchscreen_ui/include/tum_touchscreen_ui/TouchscreenWindow.h
+++ tum_touchscreen_ui/include/tum_touchscreen_ui/TouchscreenWindow.h
@@ -6,6 +6,7 @@
 #define SRC_TOUCHSCREENWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
 #include <memory>
 #include <ros/ros.h>
 #include <tum_ar_msgs/ARSlide.h>
@@ -24,6 +25,8 @@ namespace tum {
 
 			void showInterface(const tum_ar_msgs::ARSlide& slide);
 			void clearInterface();
+			static QPushButton* createButtonCopy(const QPushButton* button);
+			QPushButton* getButton(const tum_ar_msgs::Outcome& outcome);
 
 		public slots:
 			void pushButtonOutcomeClicked();
@@ -36,6 +39,12 @@ namespace tum {
 			std::unique_ptr<Ui::TouchscreenWindow> _ui;
 			ros::NodeHandle _nh;
 			ros::Publisher _userInputPub;
+
+			QPushButton* _defaultButtonTemplate;
+			QPushButton* _infoButtonTemplate;
+			QPushButton* _successButtonTemplate;
+			QPushButton* _warnButtonTemplate;
+			QPushButton* _errorButtonTemplate;
 	};
 }
 
